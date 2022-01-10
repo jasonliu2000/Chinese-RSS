7ce14e0cfbfaba7f7c3777f70881fcc0c206a3b1
Jason Liu <jasonliu00@gmail.com>
2022-01-10 15:45:57 -0500

    Initial Commit

---
+++ Chinese RSS/Chinese_RSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chinese_RSSApp.swift
+//  Chinese RSS
+//
+//  Created by Jason Liu on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Chinese_RSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
